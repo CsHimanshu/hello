77330694d7f2001e68491f89d6fb69d2e2d00f0d
himanshu <himanshus@hotstar.com>
2019-02-12 15:11:01 +0530

    Updates

---
+++ precache-manifest.d21b9361c7ba72f2c794f3b6ada0ff22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "529084a6c0c5be7b60a4",
+    "url": "/hello/static/js/runtime~main.529084a6.js"
+  },
+  {
+    "revision": "985acdcc4166ee1bb7a1",
+    "url": "/hello/static/js/main.985acdcc.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/hello/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "985acdcc4166ee1bb7a1",
+    "url": "/hello/static/css/main.003539a3.chunk.css"
+  },
+  {
+    "revision": "c8c76e563c690d620c0ed4c8fd697c03",
+    "url": "/hello/index.html"
+  }
+];
